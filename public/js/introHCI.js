799fe28ca9ba2f3486bae87d8beeba851f1bd8b6
vagrant <vagrant@precise32.(none)>
2015-02-12 21:17:23 +0000

    putting it together lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,20 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+    
+    $.get("/project/" + idNumber, dummy);
+}
+
+function dummy(result)
+{
+    console.log(result);
+
+    
+    var details = $(".project#project"+result['id']).find(".details");
+    details.html(result['summary']);
+    console.log(details);
+    //$("#project-container").html(projectHTML);
+    //$("#project-description").html(result['summary']);
 }
 
 /*
@@ -35,4 +49,17 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+    
+    $.get("/palette", randomColor);
+
+}
+
+function randomColor(result){
+    var colors = (result['colors']['hex']);
+    
+    $('body').css('background-color', colors[0]);
+    $('.thumbnail').css('background-color', colors[1]);
+    $('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+    $('p').css('color', colors[3]);
+    $('.project img').css('opacity', .75);
 }
